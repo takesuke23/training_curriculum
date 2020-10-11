a77c3da42d685af6f82c2f63ef932cbceff41b0e
takesuke23 <takesuke.23@gmail.com>
2020-10-11 14:44:41 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -23,19 +23,26 @@ class CalendarsController < ApplicationController
 
     # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
     @todays_date = Date.today
-    # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
-
+    # 例)今日が2月1日の場合・・・ Date.today.day => 1日
     @week_days = []
-
+     # 空の配列をインスタンス変数に代入
     plans = Plan.where(date: @todays_date..@todays_date + 6)
-
+     # Plansテーブルのdateカラムからwhereメソッドで必要なデータを取ってplansに代入
     7.times do |x|
       today_plans = []
       plan = plans.map do |plan|
+        # ここのmapはeach文と捉える
         today_plans.push(plan.plan) if plan.date == @todays_date + x
+        # 変数planからplanカラムの情報だけをtodays.planにpushする
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      wday_num = Date.today.wday+x #Date.today.wdayを利用して添字となる数値を得る
+      #もしもwday_numが7以上であれば、7を引く
+      if #条件式を記述
+        wday_num = wday_num - 7
+      end
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wday_num.day }
       @week_days.push(days)
+      # 39-40行目はビューに使う
     end
 
   end
